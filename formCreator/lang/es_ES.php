a44db574da0132e4dc868c1aee071689675170c6
risingisland <risingisland@yahoo.com>
2024-09-02 11:37:43 +0100

    Add files via upload

---
+++ formCreator/lang/es_ES.php
@@ -0,0 +1,50 @@
+<?php 
+
+$i18n = array(
+    'LIST' => 'Listado',
+    'ADD_NEW'=>'Agregar nuevo',
+    'SETTINGS'=>'Ajustes',
+    'EDIT'=>'Editar',
+    'DELETEELEMENT'=>'Eliminar',
+    'COFFE'=>'¿Te gusta mi trabajo? ¿Lo vas a utilizar en algún proyecto comercial?',
+    'BACKTOLIST'=>'Volver a la lista',
+    'RECIPIENT'=>'Destinatarios Correo',
+    'SENDER'=>'Correo del Remitente',
+    'CAPTCHAINFO'=>'Crea tu Captcha y copia los códigos de',
+    'SECRETKEY'=>'Clave secreta',
+    'SITEKEY'=>'Clave del sitio',
+    'SAVESETTINGS'=>'Guardar Ajustes',
+    'CREATEEDITFORM'=>'Crear/Editar formulario',
+    'LABELTYPE'=>'Tipo de etiqueta',
+    'INPUTTYPE'=>'Tipo de entrada',
+    'REQUIRED'=>'Requerida',
+    'ADDTOFORM'=>'Agregar al formulario',
+    'SAVEFORM'=>'Guardar formulario',
+    'LABELEMPTY'=>'¡La etiqueta no puede estar vacía!',
+    'QUESTION'=>'¿Estás seguro de que quieres eliminar este artículo?',
+    'PLACEHOLDERINFO'=>'El nombre del formulario no puede incluir espacios ni letras mayúsculas.',
+
+    //frontend form
+
+    'SENDMESSAGE'=>'Enviar mensaje',
+    'SENDEDMSG'=>'El mensaje ha sido enviado.',
+    'SENDEDMSGERROR'=>'Se produjo un error al enviar el mensaje.',
+    'CAPTCHAERROR'=>'¡Por favor complete la verificación CAPTCHA!',
+
+
+    //message from form
+
+    'SUBJECT'=>'¡Nuevo mensaje desde tu página web!',
+    'NEWMSG'=>'Has recibido un nuevo mensaje de tu página web:',
+    'FOOTERINFO'=>'Este correo electrónico se envió desde el formulario de contacto de su sitio web.',
+
+
+    //messagesettings
+
+    'REDIRECTQUESTION'=>'¿Redirigir éxito/error a la página?',
+    'SUCCESSPAGE'=>'Redirección de página de éxito',
+    'ERRORPAGE'=>'Error de redireccionamiento de página'
+  
+  );
+
+;?>
